e019856e0f0dd79fac8d03ca4facdc082cab2f46
FBetulDemir <fatmabetuld@gmail.com>
2024-08-22 12:56:55 +0200

    Worked on click events on script.js

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 let randomNumber = document.querySelector(".random_number");
 let message = document.querySelector(".message");
-const score = document.querySelector(".score");
-const highestScore = document.querySelector(".highest_score");
-let inputValue = document.querySelector(".input_value").value;
+
 const checkBtn = document.querySelector(".check_btn");
 const restartButton = document.querySelector(".restart_btn");
 
-console.log(inputValue);
-function startGame() {
-  score.innerHTML = 20;
-  randomNumber.innerHTML = Math.floor(Math.random() * 20 + 1);
-  checkBtn.addEventListener("click", function () {
-    if (inputValue === randomNumber) {
-      message.innerHTML = "You have won!";
+let secretNum = Math.floor(Math.random() * 20 + 1);
+let score = 20;
+let highestScore = 0;
+console.log(secretNum);
+
+checkBtn.addEventListener("click", function () {
+  const guess = Number(document.querySelector(".input_value").value);
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".highest_score").textContent = highestScore;
+
+  console.log("clicked");
+
+  if (guess === secretNum) {
+    message.textContent = "You have won!";
+    if (highestScore < score) {
+      highestScore = score;
     }
-    // inputValue === randomNumber ? "You have won!" : "Wrong number!";
-  });
-}
-startGame();
+  } else if (!guess) {
+    message.textContent = "No number, please enter a number!";
+  } else if (guess !== secretNum) {
+    message.textContent = "wrong number";
+    score--;
+    document.querySelector(".score").textContent = score;
+    if (scoreDisplay < 1) {
+      message.textContent = "You have lost!";
+    }
+  }
+});
 
 restartButton.addEventListener("click", function () {
-  startGame();
+  score = 20;
+  secretNum = Math.floor(Math.random() * 20) + 1;
+
+  message.textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".random_number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".random_number").style.width = "15rem";
 });
